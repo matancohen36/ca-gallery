992a05a483fcd3949faaf9e27bd0671614a82c66
Matan Cohen <matanco3605@gmail.com>
2020-10-11 19:20:15 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
 console.log('Starting up');
+
+function renderPortfilo (){
+    var projs = getProjs()
+    var strHtmls = projs.map(function (proj) {
+        return `
+        <div class="col-md-4 col-sm-6 portfolio-item">
+        <a onclick="renderModal('${proj.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/portfolio/${proj.name}.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${proj.name}</h4>
+          <p class="text-muted">${proj.desc}</p>
+        </div>
+        </div>
+        `
+    })
+    document.querySelector('.port-row').innerHTML = strHtmls.join('')
+}
+
+
+function renderModal(id){
+    var proj = getProjById(id)
+    var strHtml =
+    `<div class="portfolio-modal modal fade" id="portfolioModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <h2>${proj.name}</h2>
+                <p class="item-intro text-muted">${proj.name}.</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.name}.jpg" alt="">
+                <p>${proj.desc}</p>
+                <ul class="list-inline">
+                  <li>Date: ${proj.date}</li>
+                  <li>Client: Threads</li>
+                  <li>Category: ${proj.labels}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`
+document.querySelector('.modolu').innerHTML = strHtml
+
+}
+ 
+
+renderPortfilo();
---
+++ js/services/main.service.js
@@ -0,0 +1,66 @@
+var gProjs = [{
+    id: "1",
+    name: "MineSweeper",
+    title: "MineSweeper",
+    desc: "My Version of the Classic game with all the Features",
+    url: "projs/Minesweeper",
+    date: "Sep 2020",
+    labels:["Board Game","Puzzle"]
+},
+{
+    id: "2",
+    name: "In-Picture",
+    title: "In-Picture",
+    desc: "My Version of the Classic game with all the Features",
+    url: "projs/In-Picture",
+    date: "Sep 2020",
+    labels:["Puzzle"]
+},
+{
+    id: "3",
+    name: "Touch-Nums",
+    title: "Touch-Nums",
+    desc: "My Version of the Classic game with all the Features",
+    url: "projs/Touch-Nums",
+    date: "Sep 2020",
+    labels:["Board Game","Puzzle"]
+},
+{
+    id: "4",
+    name: "Pacman",
+    title: "Pacman",
+    desc: "My Version of the Classic game with all the Features",
+    url: "projs/Pacman",
+    date: "Sep 2020",
+    labels:["Board Game"]
+},
+{
+    id: "5",
+    name: "Chess",
+    title: "Chess",
+    desc: "My Version of the Classic game with all the Features",
+    url: "projs/Chess",
+    date: "Sep 2020",
+    labels:["Board Game","Matrix"]
+},
+{
+    id: "6",
+    name: "Todo",
+    title: "Todo",
+    desc: "My Version of the Todo List with all the Features",
+    url: "projs/Todo",
+    date: "Sep 2020",
+    labels:["Todo List"]
+},
+
+]
+
+function getProjs() {
+    return gProjs;
+}
+
+function getProjById(id) {
+    return gProjs.find(function (proj) {
+        return proj.id === id
+    })
+}
